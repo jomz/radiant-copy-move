49807a1a8b0337bbf50d2771ee070ac68c87027d
udzura <udzura@udzura.jp>
2010-08-04 11:55:54 +0900

    move routes to config/routes.rb and fix problems in copying a page many times into the same subdirectory.

--- copy_move_extension.rb
+++ copy_move_extension.rb
@@ -1,18 +1,19 @@
 require_dependency 'application_controller'
 
 class CopyMoveExtension < Radiant::Extension
-  version "2.0.0"
+  version "2.0.1"
   description "Adds the ability to copy and move a page and all of its children"
   url "http://gravityblast.com/projects/radiant-copymove-extension/"
 
-  define_routes do |map|
-    map.with_options(:controller => "admin/pages") do |cm|
-      cm.copy_page_admin_page     '/admin/pages/:id/copy_page',     :action => 'copy_page'
-      cm.copy_children_admin_page '/admin/pages/:id/copy_children', :action => 'copy_children'
-      cm.copy_tree_admin_page     '/admin/pages/:id/copy_tree',     :action => 'copy_tree'
-      cm.move_admin_page          '/admin/pages/:id/move',          :action => 'move'
-    end
-  end
+  # good by for define_routes
+  #define_routes do |map|
+  #  map.with_options(:controller => "admin/pages") do |cm|
+  #    cm.copy_page_admin_page     '/admin/pages/:id/copy_page',     :action => 'copy_page'
+  #    cm.copy_children_admin_page '/admin/pages/:id/copy_children', :action => 'copy_children'
+  #    cm.copy_tree_admin_page     '/admin/pages/:id/copy_tree',     :action => 'copy_tree'
+  #    cm.move_admin_page          '/admin/pages/:id/move',          :action => 'move'
+  #  end
+  #end
 
   def activate
     Admin::PagesController.class_eval do
